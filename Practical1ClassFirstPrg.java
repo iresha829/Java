b5968a87ab0261b4d9a575841aeb99530d8c3a35
Iresha Dhananjani <69219081+iresha829@users.noreply.github.com>
2021-01-23 08:11:30 +0530

    Add files via upload

---
+++ Practical1ClassFirstPrg.java
@@ -0,0 +1,19 @@
+package pkgclass.firstprg;
+public class ClassFirstPrg 
+
+{
+    public static void main(String[] args) 
+    {
+    System.out.println("Hello Word");
+    }
+    
+}
+
+class FirstPrg
+{
+    public static void main(String args[])
+    {
+        System.out.println("Dhananjani");
+        System.out.println("19.1 MIS");
+    }
+}
