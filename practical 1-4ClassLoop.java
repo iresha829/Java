e6c61f26846d60382398ddaf3ca06d0cf4fc9df3
Iresha Dhananjani <69219081+iresha829@users.noreply.github.com>
2021-01-26 07:41:41 +0530

    Add files via upload

---
+++ practical 1-4ClassLoop.java
@@ -0,0 +1,12 @@
+package pkgclass.loop;
+public class ClassLoop 
+{
+    public static void main(String[] args)
+    {
+      int a;
+      a=0;
+      while(a<=4)
+      System.out.println("Executing loop"+a,a++);
+    }
+    
+}
